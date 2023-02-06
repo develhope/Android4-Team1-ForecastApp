9b048e520a37513d96e0b97039621473cde48819
francomirko91 <francomirko91@gmail.com>
2023-02-06 18:17:06 +0100

    bottom navigation complete

---
+++ app/src/main/java/co/develhope/meteoapp/FragmentHome.kt
@@ -0,0 +1,11 @@
+package co.develhope.meteoapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FragmentHome : Fragment(R.layout.fragment_home) {
+
+}
--- app/src/main/java/co/develhope/meteoapp/MainActivity.kt
+++ app/src/main/java/co/develhope/meteoapp/MainActivity.kt
@@ -2,10 +2,45 @@ package co.develhope.meteoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import co.develhope.meteoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(FragmentHome())
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.home -> replaceFragment(FragmentHome())
+                R.id.oggi -> replaceFragment(FragmentOggi())
+                R.id.domani -> replaceFragment(FragmentDomani())
+                R.id.cerca -> replaceFragment(FragmentCerca())
+
+                else ->{
+
+                }
+            }
+            true
+
+        }
+
+    }
+
+    private fun replaceFragment (fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+
+
     }
 }
