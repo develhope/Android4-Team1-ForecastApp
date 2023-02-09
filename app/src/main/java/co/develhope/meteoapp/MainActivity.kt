100e67b3a60302b08cbc784194bafe6574d8a0db
francomirko91 <francomirko91@gmail.com>
2023-02-09 13:29:25 +0100

    bottom nav completed

--- app/src/main/java/co/develhope/meteoapp/MainActivity.kt
+++ app/src/main/java/co/develhope/meteoapp/MainActivity.kt
@@ -3,43 +3,20 @@ package co.develhope.meteoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import co.develhope.meteoapp.databinding.ActivityMainBinding
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        replaceFragment(FragmentHome())
+        setContentView(R.layout.activity_main)
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainerView)
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
-
-            when(it.itemId){
-                R.id.home -> replaceFragment(FragmentHome())
-                R.id.oggi -> replaceFragment(FragmentOggi())
-                R.id.domani -> replaceFragment(FragmentDomani())
-                R.id.cerca -> replaceFragment(FragmentCerca())
-
-                else ->{
-
-                }
-            }
-            true
-
-        }
-
-    }
-
-    private fun replaceFragment (fragment : Fragment){
-
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
-        fragmentTransaction.commit()
-
+        bottomNavigationView.setupWithNavController(navController)
 
 
     }
