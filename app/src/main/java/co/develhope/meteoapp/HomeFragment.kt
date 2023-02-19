37c95c0e6a47ffd0a567663e3415cecb93440a40
gio <ziovanava@gmail.com>
2023-02-19 20:49:20 +0100

    Home screen ver.1

--- app/src/main/java/co/develhope/meteoapp/HomeFragment.kt
+++ app/src/main/java/co/develhope/meteoapp/HomeFragment.kt
@@ -1,33 +1,21 @@
 package co.develhope.meteoapp
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.develhope.meteoapp.Data.HomeAdapter
+import co.develhope.meteoapp.Data.RecyclerElement
+import java.text.SimpleDateFormat
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var adapter : RecyclerView.Adapter<HomeAdapter.ViewHolder>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +25,60 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dateFormat = SimpleDateFormat("dd/MM", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+
+        view.findViewById<TextView>(R.id.day).text = "Oggi"
+        view.findViewById<TextView>(R.id.city).text = "Palermo, Sicilia"
+        view.findViewById<TextView>(R.id.degree).text = "13°"
+        view.findViewById<TextView>(R.id.degree2).text = "26°"
+        view.findViewById<TextView>(R.id.dataPercent).text = "3%"
+        view.findViewById<TextView>(R.id.dataKMH).text = "20kmh"
+        view.findViewById<TextView>(R.id.data).text = dateFormat.format(calendar.time)
+
+
+        fun getIconSun(weather : String ) : Int{
+            return if (weather == "sunny"){
+                R.drawable.ic_launcher_foreground
+            } else{
+                R.drawable.sun_icon
             }
+        }
+
+
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+        val list = mutableListOf<RecyclerElement>()
+        for (i in 0 until 5) {
+            val dayOfWeek = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault())
+            val capitalizedDayOfWeek = dayOfWeek.replaceFirstChar { it.uppercase() }
+            val recyclerElement = RecyclerElement(
+                capitalizedDayOfWeek,
+                dateFormat.format(calendar.time),
+                "min",
+                "max",
+                "precip.",
+                "vento",
+                getIconSun("cloudy"),
+                "20°",
+                "31°",
+                "0%",
+                "12kmh")
+            list.add(recyclerElement)
+            calendar.add(Calendar.DAY_OF_YEAR, 1)
+        }
+
+
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        adapter = HomeAdapter(list) {
+            // GESTIONE DEL CLICK DA IMPREMENTARE
+        }
+        recyclerView.adapter = adapter
     }
+
 }
+
