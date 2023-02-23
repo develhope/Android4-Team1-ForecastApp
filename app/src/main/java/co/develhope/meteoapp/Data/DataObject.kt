3ae8fa1ae1b85dfc4d6075be831e3e28150673d5
francomirko91 <francomirko91@gmail.com>
2023-02-23 12:26:06 +0100

    changed OffsetDateTime library

--- app/src/main/java/co/develhope/meteoapp/Data/DataObject.kt
+++ app/src/main/java/co/develhope/meteoapp/Data/DataObject.kt
@@ -1,34 +1,9 @@
 package co.develhope.meteoapp.Data
 
-import java.time.OffsetDateTime
+import org.threeten.bp.OffsetDateTime
+
 
 object DataObject {
-    data class TomorrowTitle(
-        val city : String,
-        val region : String,
-        val day : OffsetDateTime,
-    )
-
-    data class TomorrowRow(
-        // Row Data
-        var time: OffsetDateTime,
-        var degrees: Int,
-        var percentage: Int,
-
-        // CardView Data
-        var cvDegrees: Int,
-        var cvNumberUV: Int,
-        var cvPercentage2: Int,
-        var cvNNE: String,
-        var cvPercentage: Int,
-        var cvRainCM: Int,
-    )
-
-
-    sealed class TomorrowSealed(){
-        data class Title (val titleTomorrow : TomorrowTitle) : TomorrowSealed()
-        data class Row (val tomorrowRow: TomorrowRow) : TomorrowSealed()
-    }
 
 
 }
--- app/src/main/java/co/develhope/meteoapp/tomorrow/TomorrowFragment.kt
+++ app/src/main/java/co/develhope/meteoapp/tomorrow/TomorrowFragment.kt
@@ -6,9 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import co.develhope.meteoapp.Data.DataObject
 import co.develhope.meteoapp.databinding.FragmentTomorrowBinding
-import java.time.OffsetDateTime
+import org.threeten.bp.OffsetDateTime
+
 
 class TomorrowFragment : Fragment() {
 
@@ -35,25 +35,25 @@ class TomorrowFragment : Fragment() {
 
     }
 
-    private val titleTomorrowScreen = DataObject.TomorrowTitle("Palermo","Sicilia", OffsetDateTime.now())
+    private val titleTomorrowScreen = TomorrowTitle("Palermo","Sicilia", OffsetDateTime.now())
 
-    private val rowTomorrowScreen = DataObject.TomorrowRow(OffsetDateTime.now(),14,80,15,1/10,20,"NNE 1km/H",3,0)
-    private val rowTomorrowScreen2 = DataObject.TomorrowRow(OffsetDateTime.now().plusHours(1),15,80,13,2/10,50,"NNE 1km/h",5,10)
-    private val rowTomorrowScreen3 = DataObject.TomorrowRow(OffsetDateTime.now().plusHours(2),16,80,12,1/10,80,"NNE 1km/h",3,0)
-    private val rowTomorrowScreen4 = DataObject.TomorrowRow(OffsetDateTime.now().plusHours(3),16,80,12,1/10,80,"NNE 1km/h",3,0)
-    private val rowTomorrowScreen5 = DataObject.TomorrowRow(OffsetDateTime.now().plusHours(4),16,80,12,1/10,80,"NNE 1km/h",3,0)
-    private val rowTomorrowScreen6 = DataObject.TomorrowRow(OffsetDateTime.now().plusHours(5),17,80,12,1/10,80,"NNE 1km/h",3,0)
-    private val rowTomorrowScreen7 = DataObject.TomorrowRow(OffsetDateTime.now().plusHours(6),17,80,12,1/10,80,"NNE 1km/h",3,0)
+    private val rowTomorrowScreen = TomorrowRow(OffsetDateTime.now(),14,80,15,1/10,20,"NNE 1km/H",3,0)
+    private val rowTomorrowScreen2 = TomorrowRow(OffsetDateTime.now().plusHours(1),15,80,13,2/10,50,"NNE 1km/h",5,10)
+    private val rowTomorrowScreen3 = TomorrowRow(OffsetDateTime.now().plusHours(2),16,80,12,1/10,80,"NNE 1km/h",3,0)
+    private val rowTomorrowScreen4 = TomorrowRow(OffsetDateTime.now().plusHours(3),16,80,12,1/10,80,"NNE 1km/h",3,0)
+    private val rowTomorrowScreen5 = TomorrowRow(OffsetDateTime.now().plusHours(4),16,80,12,1/10,80,"NNE 1km/h",3,0)
+    private val rowTomorrowScreen6 = TomorrowRow(OffsetDateTime.now().plusHours(5),17,80,12,1/10,80,"NNE 1km/h",3,0)
+    private val rowTomorrowScreen7 = TomorrowRow(OffsetDateTime.now().plusHours(6),17,80,12,1/10,80,"NNE 1km/h",3,0)
 
     private val tomorrowItems = listOf(
-        DataObject.TomorrowSealed.Title(titleTomorrowScreen),
-        DataObject.TomorrowSealed.Row(rowTomorrowScreen),
-        DataObject.TomorrowSealed.Row(rowTomorrowScreen2),
-        DataObject.TomorrowSealed.Row(rowTomorrowScreen3),
-        DataObject.TomorrowSealed.Row(rowTomorrowScreen4),
-        DataObject.TomorrowSealed.Row(rowTomorrowScreen5),
-        DataObject.TomorrowSealed.Row(rowTomorrowScreen6),
-        DataObject.TomorrowSealed.Row(rowTomorrowScreen7),
+        TomorrowSealed.Title(titleTomorrowScreen),
+        TomorrowSealed.Row(rowTomorrowScreen),
+        TomorrowSealed.Row(rowTomorrowScreen2),
+        TomorrowSealed.Row(rowTomorrowScreen3),
+        TomorrowSealed.Row(rowTomorrowScreen4),
+        TomorrowSealed.Row(rowTomorrowScreen5),
+        TomorrowSealed.Row(rowTomorrowScreen6),
+        TomorrowSealed.Row(rowTomorrowScreen7),
 
     )
 
